93ce4b950b6c729e69140ae1bb5f1074370202cb
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-16 22:08:46 +0530

    SIngle Product Page Design

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,41 @@
 import React from "react";
 import Head from "next/head";
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+} from "@material-ui/core";
 import useStyles from "./../utils/styles";
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Next E-Commerce App</title>
+        <title>
+          {title ? `${title} - Next E-Commerce App` : `Next E-Commerce App`}
+          {description && <meta name="description" content={description} />}
+        </title>
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>E-Commerce</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>E-Commerce</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/cart" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
--- utils/styles.js
+++ utils/styles.js
@@ -8,8 +8,20 @@ const useStyles = makeStyles({
       marginLeft: 10,
     },
   },
+  brand: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    textDecoration: "none",
+  },
+  grow: {
+    flexGrow: 1,
+  },
   main: {
     minHeight: "80vh",
+  },
+  section:{
+    marginTop:10,
+    marginBottom: 10,
   },
   footer: {
     textAlign: "center",
