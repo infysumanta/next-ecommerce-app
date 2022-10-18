925b75f7f524109f85ef4f9aea4aa8f1db2c09cf
Sumanta Kabiraj <sumantablog@gmail.com>
2022-10-19 00:18:40 +0530

    added loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,28 @@
-import { useEffect } from 'react';
-import '../styles/globals.css';
-import { StoreProvider } from '../utils/Store';
+import { useEffect } from "react";
+import "../styles/globals.css";
+import { StoreProvider } from "../utils/Store";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
   return (
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
+    <>
+      <NextNProgress
+        color="#f0c000"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </>
   );
 }
 
